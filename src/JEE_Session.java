f2380ac1cb44071ada6e97f0db41ca04b91c2e08
sby-kov <32094353+sby-kov@users.noreply.github.com>
2020-05-05 21:07:25 +0700

    редактирован JEE_Session.java

---
+++ src/JEE_Session.java
@@ -0,0 +1,11 @@
+/**
+ * User:  Marina
+ * Дата:  05.05.2020
+ * Время: 0:08
+ */
+public class JEE_Session {
+    public static void main(String[] args) {
+        System.out.println("Всем ПРИВЕТ от Session !");
+    }
+
+}
